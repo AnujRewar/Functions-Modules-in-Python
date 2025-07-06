d5920f7a836159a04b6d0332742048f391bbadf3
mr.senior <75022117+AnujRewar@users.noreply.github.com>
2025-07-06 14:09:35 +0530

    Add files via upload

---
+++ task3_2.py
@@ -0,0 +1,6 @@
+#performing mathematical caluculation useing math library
+import math 
+number=int(input("Enter a number:"))
+print(f"square root of {number} is {math.sqrt(number)}")
+print("Natural logorithm of ", number ," is ",math.log(number))
+print("Sine value of ", number ," is", math.sin(number))
